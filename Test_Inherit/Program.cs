0d048837458b700316969ec848e1c629e502a5b4
user <user@DESKTOP-RAEKJ4K>
2017-05-31 18:44:26 +0800

    加入專案檔案。

---
+++ Test_Inherit/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Inherit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        /// <summary>
+        /// 被繼承者
+        /// </summary>
+        public class BeHeir
+        {
+
+        }
+        /// <summary>
+        /// 繼承者
+        /// </summary>
+        public class Heir : BeHeir
+        {
+
+        }
+    }
+}
